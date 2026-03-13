ddf856aa68024b2af9bdc66ee50acf2c5d5c692b
PyContriver <siddarthpydev@gmail.com>
2026-03-13 13:38:44 +0530

    fix: pre-commit

--- plugins/catalog-backend-module-rhaap/src/providers/helpers.ts
+++ plugins/catalog-backend-module-rhaap/src/providers/helpers.ts
@@ -1,13 +1,13 @@
 export function normalizeTags(tags: string[]): string[] {
-    return tags.map(tag =>
-      tag
-        .toLowerCase()
-        .replaceAll(/[^a-z0-9+#-]/g, '-')
-        .replaceAll(/-+/g, '-')
-        .replaceAll(/^-|-$/g, ''),
-    );
-  }
+  return tags.map(tag =>
+    tag
+      .toLowerCase()
+      .replaceAll(/[^a-z0-9+#-]/g, '-')
+      .replaceAll(/-+/g, '-')
+      .replaceAll(/^-|-$/g, ''),
+  );
+}
 
-  export function normalizeBaseUrl(baseUrl: string): string {
-    return baseUrl.replaceAll(/\/$/g, '');
-  }
+export function normalizeBaseUrl(baseUrl: string): string {
+  return baseUrl.replaceAll(/\/$/g, '');
+}
