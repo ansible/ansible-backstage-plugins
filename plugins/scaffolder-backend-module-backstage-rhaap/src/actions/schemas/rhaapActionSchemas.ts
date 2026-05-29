a775da195d57cbacccce08b76a413aa8e8e7286d
abhikdps <abanand@redhat.com>
2026-05-29 15:41:38 +0530

    Add support to build execution environments on gitlab

--- plugins/scaffolder-backend-module-backstage-rhaap/src/actions/schemas/rhaapActionSchemas.ts
+++ plugins/scaffolder-backend-module-backstage-rhaap/src/actions/schemas/rhaapActionSchemas.ts
@@ -179,6 +179,7 @@ export const eeDefinitionInputSchema = z
     buildImageTag: z.string().optional(),
     registryTlsVerify: z.boolean().optional(),
     owner: z.string().optional(),
+    scmProvider: z.enum(['github', 'gitlab']).optional(),
   })
   .catchall(z.unknown())
   .superRefine((data, ctx) => {
