a29e88da695a1ff71e9bf8722e9e42d7ce874478
Abhishekism9450 <32683845+Abhishekism9450@users.noreply.github.com>
2026-08-18 11:57:51 +0530

    fix: Handle pagination in job templates autocomplete to display all templates (#544)
    * Handle pagination in job templates autocomplete to display all templates
    
    (cherry picked from commit 9814ebb684fb17ec35421136cb626128bd67f988)

--- plugins/backstage-rhaap-common/src/AAPClient/AAPClient.ts
+++ plugins/backstage-rhaap-common/src/AAPClient/AAPClient.ts
@@ -893,8 +893,8 @@ export class AAPClient implements IAAPService {
     }
 
     const endPoint = `api/controller/v2/${aapResource}/?${urlSearchParams.toString()}`;
-    const response = await this.executeGetRequest(endPoint, token);
-    return await response.json();
+    const results = await this.executeCatalogRequest(endPoint, token);
+    return { results, count: results.length };
   }
 
   public async getJobTemplatesByName(
