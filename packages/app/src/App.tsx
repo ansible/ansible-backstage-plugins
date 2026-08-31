a4540f45ce726038429aef674e3a89d6542a3f5f
Sudhir Verma <9924513+sudhirverma@users.noreply.github.com>
2026-08-31 15:21:26 +0530

    chore(app): restore CssBaseline in static app shell
    Keep AppThemeFixer only on the RHDH mount point; static yarn start
    continues to use MUI CssBaseline directly.
    
    AAP-89226

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -27,7 +27,7 @@ import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
 import { GlobalHeader } from './components/GlobalHeader';
 import { getThemes } from '@red-hat-developer-hub/backstage-plugin-theme';
-import { AppThemeFixer } from '@ansible/plugin-backstage-self-service';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import {
   AlertDisplay,
@@ -148,7 +148,7 @@ const routes = (
 
 export default app.createRoot(
   <>
-    <AppThemeFixer />
+    <CssBaseline />
     <AlertDisplay />
     <OAuthRequestDialog />
     <AppRouter>
