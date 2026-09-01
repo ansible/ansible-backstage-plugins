60f6ec2785be9c76bde51bfa0f30e7754bdafa7c
Jeevansuresh1 <jes@redhat.com>
2026-09-01 17:10:18 +0530

    test(auth): fix callbackUrl in cookie tests to use realistic path for path derivation

--- plugins/auth-backend-module-rhaap-provider/src/authenticator.test.ts
+++ plugins/auth-backend-module-rhaap-provider/src/authenticator.test.ts
@@ -207,7 +207,7 @@ describe('authenticator', () => {
             clientSecret: CLIENT_SECRET,
             host: DEFAULT_HOST,
             checkSSL: CHECK_SSL,
-            callbackUrl: 'http://localhost',
+            callbackUrl: 'http://localhost/api/auth/rhaap/handler/frame',
           },
         }),
       });
@@ -285,7 +285,7 @@ describe('authenticator', () => {
             clientSecret: CLIENT_SECRET,
             host: DEFAULT_HOST,
             checkSSL: CHECK_SSL,
-            callbackUrl: 'http://localhost',
+            callbackUrl: 'http://localhost/api/auth/rhaap/handler/frame',
           },
         }),
       });
